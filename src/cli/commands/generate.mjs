daadd5388fafd1ecbe6cba2ae94496abdb1fcd70
Jakub Rożek <jakub@rozek.tech>
2021-12-17 13:14:14 +0100

    feat(cli): add --watch flag

--- src/cli/commands/generate.mjs
+++ src/cli/commands/generate.mjs
@@ -1,13 +1,13 @@
 import generate from '#core/codegen';
 
-import { readAll, write } from '../io.mjs';
+import { read, readAll, write } from '../io.mjs';
 
 export default {
   builder: yargs =>
     yargs
       .strict()
       .positional('documents', {
-        description: 'Location of JSON OpenAPI documents.',
+        description: 'Location of 3.x OpenAPI documents.',
       })
       .check(argv => {
         if (!Array.isArray(argv.documents) || argv.documents.length === 0) {
@@ -23,16 +23,37 @@ export default {
           description: 'The name of the file to store definitions as',
           type: 'string',
         },
+        quiet: {
+          alias: 'q',
+          default: false,
+          description: 'Print nothing but errors',
+          type: 'boolean',
+        },
         'ts-namespace-prefix': {
           default: '',
           description: 'Prefix used within the TypeScript declaration name',
           type: 'string',
         },
+        watch: {
+          alias: 'w',
+          default: false,
+          description: 'Watch document and rebuild on changes',
+          type: 'boolean',
+        },
       }),
   command: 'generate [documents..]',
   describe:
-    'generate Moleculer-compliant TypeScript signatures from OpenAPI documents',
+    'generate Moleculer-compliant TypeScript signatures from 3.x OpenAPI documents',
   handler: async argv => {
+    if (argv.watch) {
+      const chokidar = await import('chokidar');
+      const watcher = chokidar.watch(argv.documents);
+      watcher.on('change', async path => {
+        const { filepath, content } = await read(path);
+        await write(filepath, generate(JSON.parse(content), argv), argv);
+      });
+    }
+
     for await (const { filepath, content } of readAll(argv.documents)) {
       await write(filepath, generate(JSON.parse(content), argv), argv);
     }
--- src/cli/io.mjs
+++ src/cli/io.mjs
@@ -1,19 +1,29 @@
+import chalk from 'chalk';
 import { promises as fs } from 'node:fs';
 import * as path from 'node:path';
+import * as process from 'node:process';
 
 export async function* readAll(filepaths) {
   for (const filepath of filepaths) {
-    const obj = {
-      content: await fs.readFile(filepath, 'utf8'),
-      filepath,
-    };
-
-    yield obj;
+    yield read(filepath);
   }
 }
 
+export async function read(filepath) {
+  return {
+    content: await fs.readFile(filepath, 'utf8'),
+    filepath,
+  };
+}
+
 export async function write(filepath, input, opts) {
-  await fs.writeFile(changeBasename(filepath, opts.output), input);
+  const outputPath = changeBasename(filepath, opts.output);
+  await fs.writeFile(outputPath, input);
+  if (!opts.quiet) {
+    process.stdout.write(
+      chalk.green(`Written ${outputPath} for ${filepath}\n`),
+    );
+  }
 }
 
 function changeBasename(filepath, newBasename) {
